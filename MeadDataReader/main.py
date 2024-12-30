1e4220b35ca6e96ff6195e3041c044fb64a3610e
JacyJacka <claudiog808@gmail.com>
2024-12-30 13:22:29 +0100

    Signal example builder written, example data created and plotted

--- MeadDataReader/main.py
+++ MeadDataReader/main.py
@@ -4,15 +4,12 @@ from matplotlib import pyplot as plt
 signal = SignalBuilder("SignalExample.txt")
 signal.ReadFromFile()
 
-x_variable="time"
-y_variable="gravity"
+XLABEL="time"
+YLABEL="gravity"
 
-x_data, y_data = signal.generate_array(dimx=x_variable, dimy=y_variable)
+x_data, y_data = signal.generate_array(dimx=XLABEL, dimy=YLABEL)
 
-plt.plot(x_data, y_data, color="red", marker=".")
-plt.xlabel(x_variable)
-plt.ylabel(y_variable)
+plt.plot(x_data, y_data, color="red")
+plt.xlabel(XLABEL)
+plt.ylabel(YLABEL)
 plt.show()
-
-print(x_data)
-print(y_data)
